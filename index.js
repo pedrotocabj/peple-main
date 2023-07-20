6717d817539570a7ffa74e5f2252e03595143b57
pedrotocabj <pedrovido9@gmail.com>
2023-07-20 20:09:22 -0300

    change js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const container = document.getElementById("contenedor");
 async function getApi() {
   fetch('https://randomuser.me/api/')
     .then((response) => response.json())
-    .then((data) => createComment(data));
+    .then((data) => createComment(data))
 }
 
 function createComment(data) {
@@ -35,5 +35,36 @@ function createComment(data) {
 
 getApi();
 
+function cambiarPersona(data){
+  let name = data.results[0].name.first;
+  let surname = data.results[0].name.last;
+  let country = data.results[0].location.country;
+  let city = data.results[0].location.city;
+  let picture = data.results[0].picture.large;
 
-document.getElementById('boton').addEventListener('click', getApi)
+  let countrycode = countryList.find((pais) => {
+    return pais.name === country;
+  });
+  
+  countrycode = countrycode.code;
+
+  const cambiarTabla =document.getElementById('container');
+  cambiarTabla.innerHTML = `
+  <div id="presentation">
+  <p>${name} ${surname}</p>
+  </div>
+  <div id="image"><img src= ${picture}></div>
+  <div id="mensaje"> 
+  <p>From ${city}, ${country}</p>
+  <img src= https://www.countryflagicons.com/FLAT/64/${countrycode}.png>
+  </div>`;
+  container.append(cambiarTabla);
+}
+
+async function cambiarTexto() {
+  fetch('https://randomuser.me/api/')
+    .then((response) => response.json())
+    .then((data) => cambiarPersona(data))
+}
+
+document.getElementById('boton').addEventListener('click', cambiarTexto);
